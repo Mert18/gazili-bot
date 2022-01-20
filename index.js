2a2da29d46f42274c15034f05ce10834e8db027c
Mert18 <mertuygur02@gmail.com>
2022-01-20 23:03:19 +0300

    joke teller

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const telegramBot = require("node-telegram-bot-api");
-require("dotenv").config();
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const readLastLines = require("read-last-lines");
+require("dotenv").config();
+const fetch = require("cross-fetch");
 
 const TOKEN = process.env.TOKEN;
 
@@ -10,27 +11,10 @@ const bot = new telegramBot(TOKEN, { polling: true });
 
 const url = "https://mediko.gazi.edu.tr/view/page/20412";
 
-async function run() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-
-  await page.goto(url);
-
-  let [height, width] = await page.evaluate(() => {
-    return [
-      document.getElementsByTagName("html")[0].offsetHeight - 1500,
-      document.getElementsByTagName("html")[0].offsetWidth,
-    ];
-  });
-
-  await page.screenshot({
-    path: "screenshot.png",
-    clip: { x: 0, y: 0, width, height },
-  });
-  browser.close();
+async function generateJoke() {
+  const joker = await fetch("https://v2.jokeapi.dev/joke/Any");
+  const res = await joker.json();
+  console.log(res);
 }
 
 bot.on("message", (message) => {
@@ -77,6 +61,31 @@ bot.on("message", (message) => {
     });
   }
 
+  if (message.text == "kurt") {
+    (async () => {
+      try {
+        const res = await fetch("https://v2.jokeapi.dev/joke/Any");
+
+        if (res.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+
+        const joke = await res.json();
+        bot.sendMessage(chatid, joke.setup);
+        setTimeout(() => {
+          bot.sendMessage(chatid, joke.delivery);
+        }, 5000);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }
+
+  if (message.text == "zar") {
+    let randomZar = Math.trunc(Math.random() * 6) + 1;
+    bot.sendMessage(chatid, `${message.from.first_name}, ${randomZar} attı.`);
+  }
+
   if (message.text == "lanet olsun") {
     bot.sendMessage(
       chatid,
@@ -84,3 +93,28 @@ bot.on("message", (message) => {
     );
   }
 });
+
+/* 
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  let [height, width] = await page.evaluate(() => {
+    return [
+      document.getElementsByTagName("html")[0].offsetHeight - 1500,
+      document.getElementsByTagName("html")[0].offsetWidth,
+    ];
+  });
+
+  await page.screenshot({
+    path: "screenshot.png",
+    clip: { x: 0, y: 0, width, height },
+  });
+  browser.close();
+}
+*/
